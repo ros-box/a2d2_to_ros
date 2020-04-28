308b8ed85fc0bbcb1c4879ade8a2dd333ad3c340
Jeffrey Kane Johnson <jeffrey.kane.johnson@pm.me>
2020-04-28 16:45:49 -0400

    a2d2-to-ros -> a2d2_to_ros

---
+++ include/a2d2_to_ros/lib_a2d2_to_ros.hpp
@@ -0,0 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Mapless AI, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#ifndef A2D2_TO_ROS__LIB_A2D2_TO_ROS_HPP_
+#define A2D2_TO_ROS__LIB_A2D2_TO_ROS_HPP_
+
+namespace a2d2_to_ros {
+}  // namespace a2d2_to_ros
+
+#endif  // A2D2_TO_ROS__LIB_A2D2_TO_ROS_HPP_
