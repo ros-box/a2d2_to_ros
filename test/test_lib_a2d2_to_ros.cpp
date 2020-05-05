c75404edbd0ec2f147a56568f9d09fb383ca9536
Jeffrey Kane Johnson <jeffrey.kane.johnson@pm.me>
2020-05-05 17:05:10 -0400

    more utility functions

--- test/test_lib_a2d2_to_ros.cpp
+++ test/test_lib_a2d2_to_ros.cpp
@@ -41,6 +41,96 @@ namespace a2d2_to_ros {
 
 //------------------------------------------------------------------------------
 
+TEST(A2D2_to_ROS, frame_from_filename) {
+  {
+    const auto frame_expected = "frontcenter";
+    const auto filename =
+        "/home/maeve/data/a2d2/Ingolstadt/camera_lidar/20190401_145936/lidar/"
+        "cam_front_center/20190401145936_lidar_frontcenter_000000080.npz";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "frontcenter";
+    const auto filename = "20190401145936_lidar_frontcenter_000000080.npz";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "frontcenter";
+    const auto filename = "20190401145936_lidar_frontcenter_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "";
+    const auto filename = "20190401145936_lidar_frontcenter_sideleft_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "";
+    const auto filename = "20190401145936_lidar_front_center_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "frontleft";
+    const auto filename = "20190401145936_lidar_frontleft_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "frontright";
+    const auto filename = "20190401145936_lidar_frontright_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "sideleft";
+    const auto filename = "20190401145936_lidar_sideleft_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "sideright";
+    const auto filename = "20190401145936_lidar_sideright_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "rearcenter";
+    const auto filename = "20190401145936_lidar_rearcenter_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "";
+    const auto filename = "20190401145936_lidar_rearleft_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+
+  {
+    const auto frame_expected = "";
+    const auto filename = "20190401145936_lidar_rearright_000000080";
+    const auto frame = frame_from_filename(filename);
+    EXPECT_EQ(frame_expected, frame);
+  }
+}
+
+//------------------------------------------------------------------------------
+
 TEST(A2D2_to_ROS, flatten_2d_index) {
   /**
    * 0  1  2  3  4
